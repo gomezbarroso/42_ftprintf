9563e161b7368d8ffd042afb285f6dc4d23339a4
Alvaro Gomez Barroso <agomez-b@c3r7s6.42urduliz.com>
2022-12-22 15:41:21 +0100

    updated

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: agomez-b <agomez-b@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/30 00:02:18 by agomez-b          #+#    #+#             */
-/*   Updated: 2022/12/20 15:31:11 by agomez-b         ###   ########.fr       */
+/*   Updated: 2022/12/22 15:35:44 by agomez-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ static void	aux_def_type(va_list ap, char conv_type)
 	else if (conv_type == 'p')
 	{
 		ft_putstr_fd("0x", 1);
-		ft_puthexnbr_fd(va_arg(ap, void *), conv_type, 1);
+		ft_puthexnbr_fd(va_arg(ap, unsigned int), conv_type, 1);
 	}
 	else if (conv_type == 'd')
 		ft_putnbr_fd(va_arg(ap, int), 1);
@@ -40,5 +40,20 @@ static void	aux_def_type(va_list ap, char conv_type)
 
 int	ft_printf(char const *str, ...)
 {
+	va_list	ap;
+	size_t	i;
 
+	va_start(ap, str);
+	while (str[i] != '\0')
+	{
+		if (str[i] == '%')
+		{
+			i ++;
+			aux_def_type(ap, str[i]);
+		}
+		else
+			ft_putchar_fd(str[i], 1);
+		i ++;
+	}
+	va_end(ap);
 }
